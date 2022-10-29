1bcf7e2affd7e130e0cd91bf6c580a5a57dcb985
Furkan Civan <furkan.civan@orioninc.com>
2022-10-29 17:23:47 +0300

    Adding a Reservation entity.

---
+++ src/main/java/com/orion/labreservationapp/entity/Reservation.java
@@ -0,0 +1,29 @@
+package com.orion.labreservationapp.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "reservations")
+@Data
+public class Reservation {
+    @Id
+    Long id;
+
+    @OneToOne(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    User user;
+
+    @OneToOne(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    @JoinColumn(name = "server_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    Server server;
+
+    @Temporal(TemporalType.DATE)
+    Date reservationDate;
+}
